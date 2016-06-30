bf5e6bd55a45dbfe7020601a07a2f88161e67f18
halfshelldell <simmonslydell@gmai.com>
2016-06-30 11:28:00 -0400

    initial added controllers

---
+++ src/main/java/com/ironyard/SessionDisconnectListener.java
@@ -0,0 +1,24 @@
+package com.ironyard;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.HashMap;
+
+/**
+ * Created by illladell on 6/30/16.
+ */
+
+@Component
+public class SessionDisconnectListener implements ApplicationListener<SessionDisconnectEvent>{
+
+    @Override
+    public void onApplicationEvent(SessionDisconnectEvent event) {
+
+        String sessionId = event.getSessionId();
+        HashMap m = new HashMap();
+        m.put("id", sessionId);
+        WebSocketController.messenger.convertAndSend("/remove-player", m);
+    }
+}
